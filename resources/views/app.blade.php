4886e06f0918416c5b166d77d7bce175bc063168
chaosmos <vitovich1987@gmail.com>
2023-08-07 01:42:34 +0300

    added tailwindcss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Lara-vue3!</title>
-    @routes
-    @vite('resources/js/app.js')
-    @inertiaHead
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Larazillow</title>
+  @routes
+  @vite('resources/js/app.js')
+  @inertiaHead
 </head>
-<body>
-    @inertia
-    <div>
-        <p>
-            hello world!!!
-        </p>
-    </div>
+
+<body class="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300">
+  @inertia
 </body>
+
 </html>
