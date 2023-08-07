cfabf1ea16e8c86eeb9b4365ae635ba37d8f1af1
chaosmos <vitovich1987@gmail.com>
2023-08-08 00:35:47 +0300

    added error message

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -3,77 +3,107 @@
     <div class="grid grid-cols-6 gap-4">
       <div class="col-span-2">
         <label class="label">Beds</label>
-        <input v-model.number="form.beds" type="text" class="input" />
+        <input v-model.number="form.beds" class="input" type="text" />
         <div v-if="form.errors.beds">
           {{ form.errors.beds }}
         </div>
       </div>
-      
+
       <div class="col-span-2">
         <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Baths</label>
-        <input v-model.number="form.baths" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
+        <input
+          v-model.number="form.baths"
+          class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"
+          type="text"
+        />
         <div v-if="form.errors.baths">
           {{ form.errors.baths }}
         </div>
       </div>
-      
+
       <div class="col-span-2">
         <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Area</label>
-        <input v-model.number="form.area" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
-        <div v-if="form.errors.area">
+        <input
+          v-model.number="form.area"
+          class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"
+          type="text"
+        />
+        <div v-if="form.errors.area" class="input-error">
           {{ form.errors.area }}
         </div>
       </div>
-      
+
       <div class="col-span-4">
         <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">City</label>
-        <input v-model="form.city" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
-        <div v-if="form.errors.city">
+        <input
+          v-model="form.city"
+          class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"
+          type="text"
+        />
+        <div v-if="form.errors.city" class="input-error">
           {{ form.errors.city }}
         </div>
       </div>
-      
+
       <div class="col-span-2">
-        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Post Code</label>
-        <input v-model="form.code" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
-        <div v-if="form.errors.code">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Post
+                                                                               Code</label>
+        <input
+          v-model="form.code"
+          class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"
+          type="text"
+        />
+        <div v-if="form.errors.code" class="input-error">
           {{ form.errors.code }}
         </div>
       </div>
-      
+
       <div class="col-span-4">
         <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Street</label>
-        <input v-model="form.street" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
-        <div v-if="form.errors.street">
+        <input
+          v-model="form.street"
+          class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"
+          type="text"
+        />
+        <div v-if="form.errors.street" class="input-error">
           {{ form.errors.street }}
         </div>
       </div>
-      
+
       <div class="col-span-2">
-        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Street Nr</label>
-        <input v-model.number="form.street_nr" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
-        <div v-if="form.errors.street_nr">
+        <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Street
+                                                                               Nr</label>
+        <input
+          v-model.number="form.street_nr"
+          class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"
+          type="text"
+        />
+        <div v-if="form.errors.street_nr" class="input-error">
           {{ form.errors.street_nr }}
         </div>
       </div>
-      
+
       <div class="col-span-6">
         <label class="block mb-1 text-gray-500 dark:text-gray-300 font-medium">Price</label>
-        <input v-model.number="form.price" type="text" class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500" />
-        <div v-if="form.errors.price">
+        <input
+          v-model.number="form.price"
+          class="block w-full p-2 rounded-md shadow-sm border border-gray-300 dark:border-gray-600 text-gray-500"
+          type="text"
+        />
+        <div v-if="form.errors.price" class="input-error">
           {{ form.errors.price }}
         </div>
       </div>
-      
+
       <div class="col-span-6">
-        <button type="submit" class="btn-primary">Create</button>
+        <button class="btn-primary" type="submit">Create</button>
       </div>
     </div>
   </form>
 </template>
 
 <script setup>
-import { useForm } from '@inertiajs/vue3'
+import { useForm } from '@inertiajs/vue3';
 
 const form = useForm({
   beds: 0,
@@ -84,8 +114,8 @@ const form = useForm({
   code: null,
   street_nr: null,
   price: 0,
-})
-const create = () => form.post(route('listing.store'))
+});
+const create = () => form.post(route('listing.store'));
 </script>
 
 <style scoped>
